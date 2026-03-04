c5756b84ded20d6d2e0d19831adcb712480ee652
gurudesh prakash <prakash gurudesh>
2026-03-04 13:04:20 +0530

    Resolve merge conflict in admin/src/main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,21 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
 import { AuthProvider } from './context/AuthContext'
-=======
->>>>>>> main
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
       <AuthProvider>
         <App />
       </AuthProvider>
-=======
-      <App />
->>>>>>> main
     </BrowserRouter>
   </StrictMode>,
 )
